4ecb1f6f26d1ea25a4e5783d754d36406f597687
Yüksel Ant Özdemir <48071814+yukselexe@users.noreply.github.com>
2019-11-11 22:02:15 +0300

    Dosyalar Yüklendi

---
+++ FareTesti/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace FareTesti
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(184, 121);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 62);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Tıkla Bana!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label2.Location = new System.Drawing.Point(181, 283);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tıklama Sayısı:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label3.Location = new System.Drawing.Point(294, 283);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label1.Location = new System.Drawing.Point(340, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Son Tıklamalar:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label4.Location = new System.Drawing.Point(407, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(12, 18);
+            this.label4.TabIndex = 5;
+            this.label4.Text = ".";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label5.Location = new System.Drawing.Point(407, 82);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(12, 18);
+            this.label5.TabIndex = 6;
+            this.label5.Text = ".";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label6.Location = new System.Drawing.Point(407, 121);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(12, 18);
+            this.label6.TabIndex = 8;
+            this.label6.Text = ".";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label7.Location = new System.Drawing.Point(407, 165);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(12, 18);
+            this.label7.TabIndex = 7;
+            this.label7.Text = ".";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label8.Location = new System.Drawing.Point(407, 201);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(12, 18);
+            this.label8.TabIndex = 10;
+            this.label8.Text = ".";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label9.Location = new System.Drawing.Point(407, 240);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(12, 18);
+            this.label9.TabIndex = 9;
+            this.label9.Text = ".";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label10.Location = new System.Drawing.Point(407, 283);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(12, 18);
+            this.label10.TabIndex = 12;
+            this.label10.Text = ".";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label11.Location = new System.Drawing.Point(407, 322);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(12, 18);
+            this.label11.TabIndex = 11;
+            this.label11.Text = ".";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(325, 340);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(138, 13);
+            this.linkLabel1.TabIndex = 13;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "www.github.com/yukselexe";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(475, 359);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "10 Saniyede Fare Tıklama Testi @yukselexe ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ FareTesti/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FareTesti
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int tiklama;
+        public int sifirlamadurumu = 0;
+        public int sayacbasladimi = 0;
+        public int deneme = 0;
+
+        //yukselexe tarafından yapılmıştır...
+        public void sayacibaslat() 
+        {
+            if (sifirlamadurumu != 1)
+            {
+                if (sayacbasladimi != 1)
+                {
+                    timer1.Interval = 10000;
+                    timer1.Enabled = true;
+                    sifirlamadurumu = 1;
+                    sayacbasladimi = 1;
+                }
+                
+                
+            }
+            else
+            MessageBox.Show("Önce Sıfırlamalısın.", "Oh Shit", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e) //butona basınca
+        {
+
+            if (deneme != 1)
+            {
+                deneme = 1;
+                sayacibaslat();
+            }
+            else if (deneme == 1)
+            {
+                tiklama += 1;
+                label3.Text = Convert.ToString(tiklama);
+            }
+            
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e) //süre bitince
+        {
+            button1.Enabled = false;
+
+
+            timer1.Enabled = false;
+           
+            DialogResult cıkıs = MessageBox.Show("10 Saniyede " + tiklama + " Kere Tıkladın!", "Süre Bitti!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            
+            if (cıkıs == DialogResult.OK)
+            {
+                if (label4.Text == ".")
+                {
+                    label4.Text = Convert.ToString(tiklama);
+                }
+                else if (label5.Text == ".")
+                {
+                    label5.Text = Convert.ToString(tiklama);
+                }
+                else if (label6.Text == ".")
+                {
+                    label6.Text = Convert.ToString(tiklama);
+                }
+                else if (label7.Text == ".")
+                {
+                    label7.Text = Convert.ToString(tiklama);
+                }
+                else if (label8.Text == ".")
+                {
+                    label8.Text = Convert.ToString(tiklama);
+                }
+                else if (label9.Text == ".")
+                {
+                    label9.Text = Convert.ToString(tiklama);
+                }
+               else if (label10.Text == ".")
+                {
+                    label10.Text = Convert.ToString(tiklama);
+                }
+                else if (label11.Text == ".")
+                {
+                    label11.Text = Convert.ToString(tiklama);
+                }
+                else
+                    MessageBox.Show("Sonucun Kayıt Edilemedi \n Muhtemelen kayıt yerleri dolu! \n Programı yeniden çalıştırarak bu sorunu çözebilirsin.", "Hmm...", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+
+
+
+
+
+
+                sifirla();
+                deneme = 0;
+               
+
+
+
+
+            }
+        }
+
+
+        public void sifirla()
+        {
+            button1.Enabled = true;
+            label3.Text = "0";
+            tiklama = 0;
+            sifirlamadurumu = 0;
+            sayacbasladimi = 0;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.github.com/yukselexe");
+        }
+    }
+}
